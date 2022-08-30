212ac093b4df8762850c978b1e9b5af77f2dcf82
LatrHatr20 <111895755+LatrHatr20@users.noreply.github.com>
2022-08-30 01:47:05 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jonathan Barnes
+ *
+ * Date: 2022/08/26
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+ int main(int argc, char **argv){
+
+    printf("Hello World!");
+    printf("Jonathan Barnes");
+    printf("Computer Engineering");
+    
+    return 0;
+ }
