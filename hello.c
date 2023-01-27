fdf2a56b7eca7d4c7affa5c3af2cd7e91eb8c87b
LatrHatr20 <latrhatr20@gmail.com>
2023-01-27 10:11:51 -0600

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jonathan Barnes
+ *         jbarnes27@huskers.unl.edu
+ * Date: 2023/01/27
+ *
+ * A program that prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jon\n");
+
+  return 0;
+}
